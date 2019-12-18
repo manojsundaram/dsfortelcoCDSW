524394ac127c1cf6e10db74398ef2ff9c2abc675
root <root@ip-172-31-16-176.ap-southeast-1.compute.internal>
2019-12-18 14:07:41 +0000

    prep notes

--- prep/prepare-env.sh
+++ prep/prepare-env.sh
@@ -1,2 +1,12 @@
 sudo -u hdfs hadoop fs -chmod 777 /user
-
+hadoop fs -put ~/dsfortelcoCDSW/data/churn.all /tmp
+sudo -u hdfs hadoop  fs -chown admin:admin /tmp/churn.all
+# Add CDSW environment variables for python3 like below:
+# PYSPARK_PYTHON = 	/bin/python3.6
+# PYSPARK_DRIVER_PYTHON	= /bin/python3.6	
+hadoop fs -put data/churn.all /tmp/
+chmod 755 cdsw-build.sh
+mkdir -p models/spark
+#on all node managers
+pip3 install numpy
+pip3 install sklearn
